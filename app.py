f41717b202d442c68bd40833598dc56b7c3945ce
yuratnt <133798558+yuratnt@users.noreply.github.com>
2026-03-28 18:29:00 +0500

    process dependencies

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from flask import Flask, current_app, json, render_template, redirect, url_for, flash, request
 from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from werkzeug.exceptions import Forbidden
